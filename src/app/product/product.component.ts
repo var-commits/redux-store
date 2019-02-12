29ed2a8bc59ae163c0ebfad0776554873e90d16c
Christian Nwamba <nwambachristian@gmail.com>
2019-02-12 16:33:14 +0400

    initial

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AddToCart, RemoveFromCart } from '../store/actions';
+import { NgRedux } from '@angular-redux/store';
+import { InitialState } from '../store/reducer';
+
+export interface Product {
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+  constructor(private ngRedux: NgRedux<InitialState>) {}
+
+  inCart = false;
+  @Input() product: Product;
+
+  addToCart(item: Product) {
+      this.ngRedux.dispatch(AddToCart(item));
+      this.inCart = true;
+  }
+
+  removeFromCart(item: Product) {
+    this.ngRedux.dispatch(RemoveFromCart(item));
+    this.inCart = false;
+  }
+  ngOnInit() {}
+}
